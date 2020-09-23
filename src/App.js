d160d1f42248b5ff03a1b9832f2ab2ca333106f5
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-09-23 11:52:09 +0200

    Authentification

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ class App extends Component {
           { cards }
         </div>
         <Admin
+          pseudo={this.state.pseudo}
           recettes={this.state.recettes}
           majRecette={this.majRecette}
           ajouterRecette={this.ajouterRecette}
